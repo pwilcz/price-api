1ff731585e43ab8b6919937c6d76fff67a2b0d27
pwilcz <przemwil@gmail.com>
2023-10-28 13:31:35 +0200

    Init project

---
+++ src/main/java/com/github/pwilcz/price/PriceApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.pwilcz.price;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/pwilcz/price/PriceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.pwilcz.price;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
